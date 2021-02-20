d1f6f85a30a858b6786dd8a5f22da3793735ed9a
Derek Roode <derek.i.roode@gmail.com>
2021-02-19 19:05:16 -0500

    initial push

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -13,22 +13,19 @@ const router = express.Router();
 
 //validation for the signup
 const validateSignup = [
-  check('email')
+  check("email")
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
-  check('username')
+    .withMessage("Please provide a valid email."),
+  check("username")
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
-  check('password')
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
+    .withMessage("Password must be 6 characters or more."),
   handleValidationErrors,
 ];
 
@@ -47,7 +44,5 @@ router.post(
     });
   })
 );
-
-
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,13 +7,13 @@ router.use("/api", apiRouter);
 
 // Static routes
 // Serve React build files in production
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
   // Serve the frontend's index.html file at the root route
-  router.get('/', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
+  router.get("/", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
 
@@ -22,22 +22,19 @@ if (process.env.NODE_ENV === 'production') {
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
 }
 
-
 // Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.json({});
   });
 }
-
-
 
 module.exports = router;
