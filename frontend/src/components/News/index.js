c6c520378c0427350734f2c44078a477725f7ae4
Derek Roode <derek.i.roode@gmail.com>
2021-02-25 12:37:55 -0500

    removed meta to html

--- frontend/src/components/News/index.js
+++ frontend/src/components/News/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
-import MetaTags from 'react-meta-tags'
+
 import { Redirect } from "react-router-dom";
 import { getNews } from "../../store/news";
 
