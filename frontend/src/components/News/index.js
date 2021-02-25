23ca5938d2cfe6839a3a5a4184047a9b66dba0b1
Derek Roode <derek.i.roode@gmail.com>
2021-02-25 11:55:18 -0500

    news showing

--- frontend/src/components/News/index.js
+++ frontend/src/components/News/index.js
@@ -1,42 +1,46 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import {getNews} from '../../store/news'
+import { getNews } from "../../store/news";
 
-
-
+import "./News.css";
 
 function News() {
-  // const news = useSelector((state)=>{
-  //   return state.news.news.articles[1].source.id
-  // })
+  const news = useSelector((state) => {
+    return state?.news?.news?.articles?.map((item) => {
+      return (
+        <div className='news_news-article'>
+          <ul>
+            <a href={item.url}>
+              <li>{item.title}</li>
+              <li>
+                <img src={item.urlToImage} />
+              </li>
+            </a>
+            <li>{item.content}</li>
+          </ul>
+        </div>
+      );
+    });
+  });
 
-  const news = useSelector((state)=>{
-    return state.news.news.articles.map((item) => {
-      return <li>{item.author}</li>
-    })
-  })
-
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    dispatch(getNews())
-  
-  },[dispatch])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getNews());
+  }, [dispatch]);
 
   const sessionUser = useSelector((state) => state.session.user);
   if (!sessionUser) {
     return <Redirect to="/login" />;
   }
 
-
-  
-
-
   return (
     <div>
       <h2>Hello From News</h2>
-      <h2>{news}</h2>
+      <div className="news_news-block">
+        <div className='news_news-article'>{news}</div>
+      </div>
     </div>
   );
 }
